7217af55aad0cdd488e92b2ab79edd0dea915429
AliferDS <aliferduartesilva@gmail.com>
2021-01-25 12:56:46 -0300

    imersao react 2

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,42 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Card from '../src/components/Card'
+import Footer from '../src/components/Footer'
+import Background from '../src/components/Background'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+const Page = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <Background backgroundImage={db.bg}>
+      <Page>
+        Meu quiz
+        <Card>
+        <Card.Header>
+          Quiz
+        </Card.Header>
+        <Card.Content>
+          Este quiz ...
+        </Card.Content>
+
+        </Card>
+        <Card>
+          <Card.Content>
+            Card2
+            Este quiz ...
+          </Card.Content>
+        </Card>
+        <Footer />
+      </Page>
+    </Background>
+  )
 }
