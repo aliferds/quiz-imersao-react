4822715a461b87110fe342601fea0d7e325f4e80
AliferDS <aliferduartesilva@gmail.com>
2021-02-01 19:45:19 -0300

    adicionado quiz da galera

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import db from '../db.json'
 
 import Logo from '../src/components/Logo'
 import Card from '../src/components/Card'
@@ -15,11 +16,18 @@ const Page = styled.div`
   max-width: 350px;
   padding-top: 45px;
   margin: auto 10%;
+  h1 {
+    font-size: 1.3rem;
+    margin-bottom: 2rem;
+  }
+  li {
+    list-style: none;
+  }
   @media screen and (max-width: 500px) {
     margin: auto;
     padding: 15px;
   }
-`
+`;
 
 export default function Home() {
   const router = useRouter();
@@ -61,7 +69,19 @@ export default function Home() {
         <Card>
           <Card.Content>
             <h1>Quiz da galera</h1>
-            <p> Este quiz ... </p>
+            {db.external.map((linkExterno) => {
+              const [projectName, githubUser] = linkExterno.replace(/\//g, '')
+              .replace('https:', '')
+              .replace('.vercel.app', ' ')
+              .split('.');
+              return (
+                <li key={linkExterno}>
+                  <Card.Topic href={linkExterno}>
+                    {`${githubUser} | ${projectName}`}
+                  </Card.Topic>
+                </li>
+              );
+            })}
           </Card.Content>
         </Card>
 
